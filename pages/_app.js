282af4e790ad6b4dc25b70c6a5b3e0e323bcf588
Amaury <tecnologia.loboradio@gmail.com>
2021-05-31 16:30:44 -0500

    primera version

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Layout from '../components/Layout'
+import '../styles/globals.scss'
+import {TiendaContextProvider} from '../context/TiendaContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TiendaContextProvider>
+
+    <Layout>
+
+      <Component {...pageProps} />
+
+    </Layout>
+    
+    </TiendaContextProvider>
+  )
 }
 
 export default MyApp
